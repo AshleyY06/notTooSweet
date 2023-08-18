41cd526e45fdcb3f5c47d4a76a345054fb0f4135
AshleyY4 <14793853-AshleyY4@users.noreply.replit.com>
2023-08-18 18:25:12 +0000

    Initial commit

---
+++ allRecipes.js
@@ -0,0 +1,10 @@
+var button = $("button");
+var message = $(".message"):
+
+button.on("click",yippee);
+
+function yippee() {
+  event.preventDefault();
+  alert(`Thank you for using our site! Your feedback has been accepted!`);
+}
+
